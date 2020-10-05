095cb9d6715b209474145b20e742d903f88e10be
Zed3611 <65235883+Zed3611@users.noreply.github.com>
2020-10-05 22:03:44 +0400

    title fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../style/index.css'
 import '../style/LoadIcon.css'
+import React from "react";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>PaymentTerminal</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
